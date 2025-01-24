b27ee20f8c5696602069aeb3c998c1635ae16c83
pswbuild <pswbuild@oppo.com>
2025-01-24 16:08:22 +0800

    Synchronize code for OnePlus PKR110_15.0.0.408(CN01) Based on QCOM release TAG: kernel/build:AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.042 platform/vendor/nxp/opensource/driver:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.112 platform/vendor/opensource/camera-devicetree:AU_TECHPACK_CAMERA.LA.5.0.R1.00.00.00.000.030 platform/vendor/opensource/camera-kernel:AU_TECHPACK_CAMERA.LA.5.0.R1.00.00.00.000.030 platform/vendor/opensource/dataipa:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058 platform/vendor/opensource/display-devicetree:AU_TECHPACK_DISPLAY.LA.5.0.R1.00.00.00.000.034 platform/vendor/opensource/display-drivers:AU_TECHPACK_DISPLAY.LA.5.0.R1.00.00.00.000.034 platform/vendor/opensource/eva-devicetree:AU_TECHPACK_CV.LA.2.1.R1.00.00.00.000.021 platform/vendor/opensource/eva-kernel:AU_TECHPACK_CV.LA.2.1.R1.00.00.00.000.021 platform/vendor/opensource/mm-devicetree:AU_TECHPACK_DISPLAY.LA.5.0.R1.00.00.00.000.046 platform/vendor/opensource/mm-drivers:AU_TECHPACK_DISPLAY.LA.5.0.R1.00.00.00.000.034 platform/vendor/opensource/mmrm-devicetree:AU_TECHPACK_VIDEO.LA.5.0.R1.00.00.00.000.036 platform/vendor/opensource/mmrm-driver:AU_TECHPACK_VIDEO.LA.5.0.R1.00.00.00.000.036 platform/vendor/opensource/synx-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058 platform/vendor/opensource/video-devicetree:AU_TECHPACK_VIDEO.LA.5.0.R1.00.00.00.000.019 platform/vendor/opensource/video-driver:AU_TECHPACK_VIDEO.LA.5.0.R1.00.00.00.000.019 platform/vendor/proprietary/mm-devicetree:AU_TECHPACK_DISPLAY.LA.5.0.R1.00.00.00.000.046 platform/vendor/qcom-opensource/bt-devicetree:AU_TECHPACK_BTFM.LA.1.0.R1.00.00.00.000.026 platform/vendor/qcom-opensource/bt-kernel:AU_TECHPACK_BTFM.LA.1.0.R1.00.00.00.000.026 platform/vendor/qcom-opensource/data-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058 platform/vendor/qcom-opensource/data-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.112 platform/vendor/qcom-opensource/devicetree:AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.041 platform/vendor/qcom-opensource/eSE-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.100 platform/vendor/qcom-opensource/nfc-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058 platform/vendor/qcom-opensource/wlan/fw-api:AU_TECHPACK_WLAN.LA.1.0.R1.00.00.00.000.038 platform/vendor/qcom-opensource/wlan/platform:AU_TECHPACK_WLAN.LA.1.0.R1.00.00.00.000.038 platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn:AU_TECHPACK_WLAN.LA.1.0.R1.00.00.00.000.038 platform/vendor/qcom-opensource/wlan/qcacld-3.0:AU_TECHPACK_WLAN.LA.1.0.R1.00.00.00.000.038 platform/vendor/qcom-opensource/wlan/utils/sigma-dut:AU_TECHPACK_WLAN.LA.1.0.R1.00.00.00.000.038 platform/vendor/qcom-opensource/wlan/wlan-devicetree:AU_TECHPACK_WLAN.LA.1.0.R1.00.00.00.000.038 platform/vendor/qcom-proprietary/display-devicetree:AU_TECHPACK_DISPLAY.LA.5.0.R1.00.00.00.000.034 platform/vendor/qcom/opensource/audio-devicetree:AU_TECHPACK_AUDIO_HANDSET.LA.10.0.R1.00.00.00.000.025 platform/vendor/qcom/opensource/audio-kernel-ar:AU_TECHPACK_AUDIO_HANDSET.LA.10.0.R1.00.00.00.000.025 platform/vendor/qcom/opensource/datarmnet:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058 platform/vendor/qcom/opensource/datarmnet-ext:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058 platform/vendor/qcom/opensource/graphics-devicetree:AU_TECHPACK_GRAPHICS.LA.15.0.R1.00.00.00.000.026 platform/vendor/qcom/opensource/graphics-kernel:AU_TECHPACK_GRAPHICS.LA.15.0.R1.00.00.00.000.026 platform/vendor/qcom/opensource/securemsm-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.0.R1.11.00.00.1168.058

---
+++ vendor/qcom/opensource/camera-devicetree/config/sun.mk
@@ -0,0 +1,10 @@
+dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
+# dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
+# 				sun-camera-sensor-rumi.dtbo \
+# 				sun-camera-sensor-cdp.dtbo  \
+# 				sun-camera-sensor-qrd.dtbo
+
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/hummer-camera-overlay-evb.dtbo \
+			     oplus/hummer-camera-overlay-T0.dtbo
+
